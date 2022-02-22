b21b63c6510144aca94da2f7b01abd063b337a99
Saniya Baghirova <saniya.ibraimova@gmail.com>
2022-02-22 20:57:25 +0100

    GET /api/questions

--- client/index.jsx
+++ client/index.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+function FrontPage() {
+    return <div>
+        <h1>Welcome to Quiz</h1>
+        <div>
+            <Link to={"/questions"}>Take quiz</Link>
+        </div>
+    </div>;
+}
 
 function Application() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<FrontPage />}/>
-                <Route path={"/questions"} element={<Quiz/>}/>
+                <Route path={"/questions"} element={<h1>Welcome to quiz</h1>}/>
+                <Route path={"/*"} element={<h1>Not found</h1>}/>
             </Routes>
         </BrowserRouter>
     );
--- server/server.js
+++ server/server.js
@@ -6,4 +6,11 @@ const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on http://localhost:${server.address().port}`);
 });
 
+app.get("/api/questions", (req, res) => {
+    res.json({
+        question: "Example question",
+        answer: "Example answer",
+    });
+});
+
 app.use(express.static("../client/dist"));
