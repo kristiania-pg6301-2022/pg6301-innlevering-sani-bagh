414730dcbafc1a99af54bf73e8145736cbd788cd
Saniya Baghirova <saniya.ibraimova@gmail.com>
2022-02-23 22:31:23 +0100

    Questions, check correct answer

--- client/index.jsx
+++ client/index.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+import {Questions, randomQuestion} from "./questions";
 
 function FrontPage() {
     return <div>
         <h1>Welcome to Quiz</h1>
         <div>
-            <Link to={"/questions"}>Take quiz</Link>
+            <ul>
+                <li><Link to={"/questions"}>Take quiz</Link></li>
+                <li><Link to={"/answers"}>All questions with answers</Link></li>
+            </ul>
         </div>
+    </div>;
+}
+
+function Quiz() {
+    const [question, setQuestion] = useState(randomQuestion());
+    const [answer, setAnswer] = useState();
+
+    if (answer) {
+        return <h1>{question.options[answer] === question.answer ? "The answer is correct" : "The answer is wrong"}</h1>;
+    }
+
+
+    return <div>
+        <h1>{question.question}</h1>
+        {Object.keys(question.options)
+            .filter(option => question.options[option])
+            .map(option => <p key={option}>
+                <button onClick={() => setAnswer(option)}>{question.options[option]}</button>
+            </p> )
+        }
     </div>;
 }
 
@@ -16,7 +41,7 @@ function Application() {
         <BrowserRouter>
             <Routes>
                 <Route path={"/"} element={<FrontPage />}/>
-                <Route path={"/questions"} element={<h1>Welcome to quiz</h1>}/>
+                <Route path={"/questions"} element={<Quiz/>}/>
                 <Route path={"/*"} element={<h1>Not found</h1>}/>
             </Routes>
         </BrowserRouter>
